f2040259e99811074fa8dbdddfbc4e040990f1eb
sopheak <sansopheak686@gmail.com>
2026-02-16 17:48:56 +0700

    config base url

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+const basename =
+  import.meta.env.VITE_DEPLOY_ENV === "gh-pages" ? "/My-Portfolio-SanSopheak-" : "/";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App basename={basename} />
   </StrictMode>,
 )
